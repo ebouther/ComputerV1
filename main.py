7d98f7804b3627ca823f388d01aad63a153d7b11
ebouther <ebouther@student.42.fr>
2017-05-11 23:13:42 +0200

    input verification + step by step calc verbose

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import sys;
+import os;
+import re;
 
+def my_abs(x):
+    return x if x >= 0 else -x;
+
+# Babylonian method
 def sqrt(x):
-    last_guess = x / 2.0;
+    last_approx = x / 2.0;
     while True:
-        guess = (last_guess + x / last_guess) / 2;
-        if abs(guess - last_guess) < .000001:
-            return guess;
-        last_guess= guess;
+        approx = (last_approx + x / last_approx) / 2;
+        if my_abs(approx - last_approx) < .00001:
+            return approx;
+        last_approx = approx;
 
 def splitInBlk(eq):
     blks = [];
@@ -45,14 +51,13 @@ def simplifyEq(eq):
 
 
 def cleanEqPow(blks):
-
     for i in range(len(blks) - 1, -1, -1):
         new_arr = [];
         mult = 1;
         exp = 0;
         for nb in ''.join(blks[i][0]).split("*"):
 
-            if (nb.find("X") > -1): # should set degree here.
+            if (nb.find("X") > -1):
                 exp += float(nb[2:]);
             else:
                 mult *= float(nb);
@@ -66,12 +71,12 @@ def cleanEqPow(blks):
         if len(new_arr) == 0:
             new_arr.extend('1');
         if (mult == 0):
-            print("POP I : {}".format(blks[i]));
             blks.pop(i);
         else:
             blks[i][2] = exp;
             if (exp > 2):
-                raise ValueError('Only solve degree 2 or less polynomials');
+                print('Only solves degree 2 or less polynomials');
+                os._exit(1)
             blks[i][0] = new_arr;
     pass;
 
@@ -103,22 +108,34 @@ def calc(blks):
                 res += float(''.join(blk[0])) * -1;
             else:
                 res += float(''.join(blk[0]));
-        # print("RES : " + str(res) + " | exp : " + str(blocks[0][2]));
         if (blocks[0][2] == 0):
             blks.append(res);
         else:
             blks.append([res, blocks[0][2]]);
     return blks;
 
-def printReducedForm(eq):
-    print("Reduced form: ", end='');
+def printEq(eq):
+    print("\033[36m\t       => ", end='');
     for i in range(0, len(eq[0])):
-        print(eq[0][i][1] + ' ' + ''.join(eq[0][i][0]), end=' ');
+        print(eq[0][i][0], end=' ');
+        if eq[0][i][1] > 0:
+            print("* X^{}".format(eq[0][i][1]), end=' ');
+    print(" = ", end="");
+    for i in range(0, len(eq[1])):
+        print(eq[1][i], end=' ');
+    print("\033[0m\n");
+    pass;
+
+def printReducedForm(eq):
+    print("\n\033[36mReduced form: ", end='');
+    for i in range(0, len(eq[0])):
+
+        print((eq[0][i][1] if not (i == 0 and eq[0][i][1] == '+') else '') + ' ' + ''.join(eq[0][i][0]), end=' ');
         if eq[0][i][2] > 0:
-            print(" * X^{}".format(eq[0][i][2]), end=' ');
+            print("* X^{}".format(eq[0][i][2]), end=' ');
     for i in range(0, len(eq[1])):
         print(('+' if eq[1][i][1] == '-' else '-') + ' ' + ''.join(eq[1][i][0]), end=' ');
-    print("= 0");
+    print("= 0\033[0m\n");
     pass;
 
 
@@ -133,42 +150,50 @@ def solveQuadratic(eq):
         elif blk[1] == 1:
             b = blk[0];
 
-    print("A = {}".format(a));
-    print("B = {}".format(b));
-    print("C = {}".format(c));
+    print("a: {}".format(a));
+    print("b: {}".format(b));
+    print("c: {}\n".format(c));
 
     delta = (b * b) - (4 * a * c);
-    print("delta: ");
-    print(delta);
+    print("𝚫 = b² - 4ac");
+    print("𝚫 = ({})² - 4 * ({}) * ({})".format(b, a, c));
+    print("𝚫 = {}\n\033[36m".format(delta));
 
     if delta > 0:
+        print("𝚫 > 0");
+        print("Two solutions:");
+        print("X0 = (-b - sqrt(𝚫)) / 2a");
+        print("X0 = (-({}) - sqrt({})) / (2 * ({}))".format(b, delta, a));
+
+        print("X1 = (-b + sqrt(𝚫)) / 2a");
+        print("X1 = (-({}) + sqrt({})) / (2 * ({}))".format(b, delta, a));
         sol1 = ((-b) - sqrt(delta)) / (2 * a);
         sol2 = ((-b) + sqrt(delta)) / (2 * a);
-        print("X0 = ", end='');
-        print(sol1);
-        print("X1 = ", end='');
-        print(sol2);
+        print("\033[32m\tX0 = {:.5f}".format(sol1));
+        print("\tX1 = {:.5f}\033[0m".format(sol2));
     elif delta == 0:
+        print("𝚫 = 0");
+        print("1 solution:");
+        print("X = -b / 2a");
+        print("X = -({}) / (2 * ({}))".format(b, a));
         sol1 = (-b) / (2 * a);
-        print("X = ", end='');
-        print(sol1);
+        print("\t\033[32mX = {}\033[0m".format(sol1));
     else:
-        # delta <=> (ir)2
+        print("𝚫 < 0");
         print("No real solution.");
-        print("Complex solutions :");
+        print("2 complex solutions :");
 
         real = -b / (2 * a);
         imaginary = sqrt(-delta) / (2 * a);
-        print("sol1 = {} + {}i".format(real, imaginary));
+        print("\t\033[32mX0 = {:.5f} + {:.5f}i".format(real, imaginary));
 
         real = -b / (2 * a);
         imaginary = -sqrt(-delta) / (2 * a);
-        print("sol2 = {} + {}i".format(real, imaginary));
+        print("\tX1 = {:.5f} + {:.5f}i\033[0m".format(real, imaginary));
     pass;
 
 def solveAffine(eq):
-    print("X = ", end='');
-    print(eq[1][0] / eq[0][0][0]);
+    print("\033[32mX = {:.5f}\033[0m".format(eq[1][0] / eq[0][0][0]));
     pass;
 
 def solveEq(eq):
@@ -185,26 +210,12 @@ def solveEq(eq):
         simplifyEq(eq[0]);
         simplifyEq(eq[1]);
 
-        print();
         printReducedForm(eq);
-
-        print ("\n=======");
-        for i in range(0, len(eq[0])):
-            print(eq[0][i], end='');
-        print(" = ", end='');
-        for i in range(0, len(eq[1])):
-            print(eq[1][i], end='');
 
         eq[0] = calc(eq[0]);
         eq[1] = calc(eq[1]);
 
-        print ("\n=======");
-        for i in range(0, len(eq[0])):
-            print(eq[0][i], end='');
-        print(" = ", end='');
-        for i in range(0, len(eq[1])):
-            print(eq[1][i], end='');
-        print();
+        printEq(eq);
 
         max_degree = 0;
         for blk in eq[0]:
@@ -218,15 +229,23 @@ def solveEq(eq):
             print("True for all X");
         else:
             print("No solution");
+    else:
+        print("Bad input.")
     pass;
 
-def argsToEq(args):
-    eq = ''.join(args).split('=');
-    return eq;
-
 def main(argv):
-    argv.pop(0);
-    solveEq(argsToEq(argv));
+    if (argv[1]):
+        reg=re.compile('^[0-9 X\+\-\=\^\*\t\n]+$');
+        if (reg.match(argv[1])):
+            try:
+                solveEq(argv[1].split('='));
+            except:
+                print("Bad input.");
+                os._exit(1);
+        else:
+            print("Bad input.")
+    else:
+        print("Please pass equation as argument.")
     pass;
 
 if __name__ == "__main__":
